6a2f5270dbb851d977a5abeab2044b6eed21735e
Kurai András <k.andras9642@gmail.com>
2019-03-21 20:27:56 +0100

    Added swagger

--- Cocorico.RazorComponents/Startup.cs
+++ Cocorico.RazorComponents/Startup.cs
@@ -1,20 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Cocorico.RazorComponents.Components;
 using Cocorico.RazorComponents.Services;
+using Microsoft.Extensions.Configuration;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Cocorico.RazorComponents
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,6 +26,9 @@ namespace Cocorico.RazorComponents
                 .AddNewtonsoftJson();
 
             services.AddRazorComponents();
+
+            //TODO: Enable Swagger
+            //services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" }));
 
             services.AddSingleton<WeatherForecastService>();
         }
@@ -33,6 +39,11 @@ namespace Cocorico.RazorComponents
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                //TODO: Enable Swagger
+                //https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/1068
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
             }
             else
             {
@@ -42,6 +53,8 @@ namespace Cocorico.RazorComponents
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseMvc(routes => routes.MapRoute("default", "{controller}/{action}/{id?}"));
 
             app.UseRouting(routes =>
             {

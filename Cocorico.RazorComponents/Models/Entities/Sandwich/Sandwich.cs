40d681a778275550689d1eee73cb771d7d2bde08
Kurai András <k.andras9642@gmail.com>
2019-03-25 19:10:24 +0100

    Added Db Context, and related entities

---
+++ Cocorico.RazorComponents/Models/Entities/Sandwich/Sandwich.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cocorico.RazorComponents.Models.Entities.Sandwich
+{
+    public class Sandwich
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}

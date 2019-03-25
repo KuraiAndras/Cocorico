06e5a1ddeb427d694dde8ef200abdcd2af666f16
Kurai András <k.andras9642@gmail.com>
2019-03-25 23:44:27 +0100

    Deleted old projects

--- Cocorico.Server/Model/Entities/CocoricoUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Cocorico.Server.Model.Entities
-{
-    public class CocoricoUser : IdentityUser
-    {
-        public string Name { get; set;}
-    }
-}

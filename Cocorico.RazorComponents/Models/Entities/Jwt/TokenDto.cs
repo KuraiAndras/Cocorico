7b1b962edef6212800d1d79e60c2c5730537bacc
Kurai András <k.andras9642@gmail.com>
2019-03-28 00:55:53 +0100

    Deleted old project

--- Cocorico.RazorComponents/Models/Entities/Jwt/TokenDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Cocorico.RazorComponents.Models.Entities.Jwt
-{
-    public class TokenDto
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}

c282e292e8223937392e0c13ac0d6d460fc57a90
Kurai András <k.andras9642@gmail.com>
2019-04-08 18:55:02 +0200

    Service Result class

---
+++ Cocorico.Server/Helpers/ServiceResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Cocorico.Shared.Helpers;
+
+namespace Cocorico.Server.Helpers
+{
+    public class ServiceResult<T> : ServiceResult
+    {
+        private readonly T _data;
+
+        public T Data => IsSuccessful ? _data : throw Exception;
+
+        public ServiceResult(T data, string message = Messages.Ok)
+            : base(message)
+        {
+            _data = data;
+        }
+
+        public ServiceResult(Exception exception)
+            : base(exception)
+        {
+        }
+    }
+
+    public class ServiceResult
+    {
+        public bool IsSuccessful { get; }
+
+        public Exception Exception { get; }
+
+        public string Message { get; }
+
+        public ServiceResult()
+            : this(Messages.Ok)
+        {
+        }
+
+        public ServiceResult(string message)
+            : this(true, message)
+        {
+        }
+
+        public ServiceResult(Exception exception)
+            : this(false, string.Empty)
+        {
+            Exception = exception;
+        }
+
+        private ServiceResult(bool isSuccess, string message)
+        {
+            IsSuccessful = isSuccess;
+            Message = message;
+        }
+    }
+}
---
+++ Cocorico.Shared/Helpers/Messages.cs
@@ -0,0 +1,8 @@
+﻿namespace Cocorico.Shared.Helpers
+{
+    public static class Messages
+    {
+        public const string NotFound = "Not Found";
+        public const string Ok = "Ok";
+    }
+}

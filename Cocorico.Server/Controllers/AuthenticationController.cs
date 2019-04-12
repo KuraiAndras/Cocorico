ae6abe7c15db29ace8840b4a3c8d4c64f0a85992
Unknown <k.andras9642@gmail.com>
2019-04-12 16:56:31 +0200

    API controllers, naming fixes

--- Cocorico.Server/Controllers/AuthenticationController.cs
+++ Cocorico.Server/Controllers/AuthenticationController.cs
@@ -1,55 +1,47 @@
-﻿using Cocorico.Server.Services.Authentication;
+﻿using Cocorico.Server.Extensions;
+using Cocorico.Server.Helpers;
+using Cocorico.Server.Services.Authentication;
 using Cocorico.Shared.Dtos.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Cocorico.Server.Helpers;
 
 namespace Cocorico.Server.Controllers
 {
     [Produces("application/json")]
     [Route("api/[controller]")]
+    [ApiController]
     public class AuthenticationController : ControllerBase
     {
-        private readonly ICustomAuthenticationService _customAuthenticationService;
+        private readonly IAuthenticationService _authenticationService;
 
-        public AuthenticationController(ICustomAuthenticationService customAuthenticationService)
-        {
-            _customAuthenticationService = customAuthenticationService;
-        }
+        public AuthenticationController(IAuthenticationService authenticationService) => _authenticationService = authenticationService;
 
         [AllowAnonymous]
         [HttpPost(nameof(Login))]
         public async Task<IActionResult> Login([FromBody] LoginDetails credentials)
         {
-            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var result = await _authenticationService.LoginAsync(credentials);
 
-            var result = await _customAuthenticationService.LoginAsync(credentials);
-            //TODO: Handle fail
-
-            return new JsonResult(result);
+            return result.ToActionResult();
         }
 
         [AllowAnonymous]
         [HttpPost(nameof(Register))]
         public async Task<IActionResult> Register([FromBody] RegisterDetails model)
         {
-            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var result = await _authenticationService.RegisterAsync(model);
 
-            await _customAuthenticationService.RegisterAsync(model);
-            //TODO: Handle fail
-
-            return Ok();
+            return result.ToActionResult();
         }
 
         [Authorize(Policy = Policies.User)]
-        [HttpPost(nameof(Logout))]
+        [HttpPost(nameof(Register))]
         public async Task<IActionResult> Logout()
         {
-            await _customAuthenticationService.LogoutAsync();
-            //TODO: Handle fail
+            var result = await _authenticationService.LogoutAsync();
 
-            return Ok();
+            return result.ToActionResult();
         }
     }
 }
---
+++ Cocorico.Server/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using Cocorico.Shared.Dtos.Authentication;
+using System.Threading.Tasks;
+using Cocorico.Server.Helpers;
+
+namespace Cocorico.Server.Services.Authentication
+{
+    public interface IAuthenticationService
+    {
+        Task<IServiceResult> RegisterAsync(RegisterDetails model);
+        Task<IServiceResult<LoginResult>> LoginAsync(LoginDetails model);
+        Task<IServiceResult> LogoutAsync();
+    }
+}

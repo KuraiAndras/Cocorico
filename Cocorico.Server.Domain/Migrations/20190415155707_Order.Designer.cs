12666671f838e7a21b11b6c4e98eb940d0070203
Kurai András <k.andras9642@gmail.com>
2019-04-15 18:01:48 +0200

    added migration

--- Cocorico.Server.Domain/Migrations/20190415155707_Order.Designer.cs
+++ Cocorico.Server.Domain/Migrations/20190415155707_Order.Designer.cs
@@ -4,14 +4,16 @@ using Cocorico.Server.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cocorico.Server.Domain.Migrations
 {
     [DbContext(typeof(CocoricoDbContext))]
-    partial class CocoricoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190415155707_Order")]
+    partial class Order
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

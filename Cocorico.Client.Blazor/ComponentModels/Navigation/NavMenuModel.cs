adc42bd7accaa5ae666d2936c94dee2306107e04
Kurai András <k.andras9642@gmail.com>
2019-04-19 18:40:59 +0200

    Fixed breaking changes

--- Cocorico.Client.Blazor/ComponentModels/Navigation/NavMenuModel.cs
+++ Cocorico.Client.Blazor/ComponentModels/Navigation/NavMenuModel.cs
@@ -2,7 +2,6 @@
 using Cocorico.Shared.Helpers;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Layouts;
-using Microsoft.AspNetCore.Components.Services;
 using System.Linq;
 using System.Threading.Tasks;
 

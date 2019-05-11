9c44c02179beeaef1932fbe7019bead6c7bc044d
Kurai András <k.andras9642@gmail.com>
2019-05-11 17:01:39 +0200

    Auth controller, services

--- Cocorico.Client.Domain/Services/Authentication/ICocoricoClientAuthenticationService.cs
+++ Cocorico.Client.Domain/Services/Authentication/ICocoricoClientAuthenticationService.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Cocorico.Shared.Dtos.Authentication;
+using Cocorico.Shared.Services.Helpers;
+using System;
 using System.Collections.Generic;
-using Cocorico.Shared.Services;
+using System.Threading.Tasks;
 
 namespace Cocorico.Client.Domain.Services.Authentication
 {
-    public interface ICocoricoClientAuthenticationService : ICocoricoAuthenticationService
+    public interface ICocoricoClientAuthenticationService
     {
+        Task<IServiceResult> RegisterAsync(RegisterDetails model);
+        Task<IServiceResult<LoginResult>> LoginAsync(LoginDetails model);
+        Task<IServiceResult> LogoutAsync();
+        Task<IServiceResult> AddClaimToUserAsync(UserClaimRequest userClaimRequest);
+        Task<IServiceResult> RemoveClaimFromUserAsync(UserClaimRequest userClaimRequest);
+
         IEnumerable<string> Claims { get; }
 
         event Action UserLoggedIn;

d10afe5ad5d0eb3d943019a4cba5e3bde3b71c84
Kurai András <k.andras9642@gmail.com>
2019-05-22 20:09:56 +0200

    Added Ingredient

--- Cocorico.Server.Domain/Models/CocoricoDbContext.cs
+++ Cocorico.Server.Domain/Models/CocoricoDbContext.cs
@@ -12,6 +12,7 @@ namespace Cocorico.Server.Domain.Models
 
         public DbSet<Sandwich> Sandwiches { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -41,6 +42,11 @@ namespace Cocorico.Server.Domain.Models
                 .Entity<Order>()
                 .Property(o => o.State)
                 .HasConversion<int>();
+
+            builder
+                .Entity<Ingredient>()
+                .HasQueryFilter(i => !i.IsDeleted)
+                .HasIndex(i => new { i.Id, i.Name });
         }
     }
 }
---
+++ Cocorico.Server.Domain/Models/Entities/Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cocorico.Server.Domain.Models.Entities
+{
+    public class Ingredient : IDbEntity<int>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; }
+    }
+}

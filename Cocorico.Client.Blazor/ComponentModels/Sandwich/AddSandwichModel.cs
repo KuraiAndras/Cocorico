24e8d290d474f937064c3fcbe1cab91606e009d0
Kurai András <k.andras9642@gmail.com>
2019-05-24 02:01:25 +0200

    Client sort of works

--- Cocorico.Client.Blazor/ComponentModels/Sandwich/AddSandwichModel.cs
+++ Cocorico.Client.Blazor/ComponentModels/Sandwich/AddSandwichModel.cs
@@ -1,8 +1,10 @@
 using Cocorico.Client.Domain.Extensions;
 using Cocorico.Client.Domain.Helpers;
+using Cocorico.Shared.Dtos.Ingredient;
 using Cocorico.Shared.Dtos.Sandwich;
 using Cocorico.Shared.Helpers;
 using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Cocorico.Client.Blazor.ComponentModels.Sandwich
@@ -15,14 +17,57 @@ namespace Cocorico.Client.Blazor.ComponentModels.Sandwich
         // ReSharper disable once UnusedAutoPropertyAccessor.Local
         [Inject] private ISandwichClient SandwichHttpClient { get; set; }
 
-        protected SandwichAddDto NewSandwichDto { get; } = new SandwichAddDto();
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        [Inject] private IIngredientClient IngredientClient { get; set; }
+
+        protected SandwichAddDto NewSandwichDto { get; } = new SandwichAddDto { Ingredients = new List<IngredientDto>() };
+
+        protected List<IngredientDto> AvailableIngredients { get; private set; } = new List<IngredientDto>();
+
+        private List<IngredientDto> AddedIngredients { get; } = new List<IngredientDto>();
+
+        protected override async Task OnInitAsync()
+        {
+            try
+            {
+                var ingredients = await IngredientClient.GetAllAsync();
+
+                AvailableIngredients.AddRange(ingredients);
+            }
+            catch (SwaggerException)
+            {
+                //TODO: Handle fail
+            }
+        }
+
+
+        protected void AddIngredient(IngredientDto ingredient)
+        {
+            AddedIngredients.Add(ingredient);
+            NewSandwichDto.Ingredients = AddedIngredients;
+        }
+
+        protected void RemoveIngredient(IngredientDto ingredient)
+        {
+            AddedIngredients.Remove(ingredient);
+            NewSandwichDto.Ingredients = AddedIngredients;
+        }
 
         protected async Task Add()
         {
-            var result = await SandwichHttpClient.AddAsync(NewSandwichDto);
+            try
+            {
+                NewSandwichDto.Ingredients = AddedIngredients;
 
-            if (result.IsSuccessfulStatusCode()) UriHelper.NavigateTo(Urls.Client.Sandwiches);
-            //TODO: Handle fail
+                var result = await SandwichHttpClient.AddAsync(NewSandwichDto);
+
+                if (result.IsSuccessfulStatusCode()) UriHelper.NavigateTo(Urls.Client.Sandwiches);
+                //TODO: Handle fail
+            }
+            catch (SwaggerException)
+            {
+                //TODO: Handle fail
+            }
         }
     }
 }
--- Cocorico.Client.Blazor/ComponentModels/Sandwich/EditSandwichModel.cs
+++ Cocorico.Client.Blazor/ComponentModels/Sandwich/EditSandwichModel.cs
@@ -1,16 +1,18 @@
-﻿using Cocorico.Client.Domain.Extensions;
+﻿using System.Collections.Generic;
+using Cocorico.Client.Domain.Extensions;
 using Cocorico.Client.Domain.Helpers;
 using Cocorico.Shared.Dtos.Sandwich;
 using Cocorico.Shared.Helpers;
 using Microsoft.AspNetCore.Components;
 using System.Threading.Tasks;
+using Cocorico.Shared.Dtos.Ingredient;
 
 namespace Cocorico.Client.Blazor.ComponentModels.Sandwich
 {
     public class EditSandwichModel : ComponentBase
     {
         // ReSharper disable once UnusedAutoPropertyAccessor.Local
-        [Parameter] private int SandwichId { get; set; }
+        [Parameter] private int Id { get; set; }
 
         // ReSharper disable once UnusedAutoPropertyAccessor.Local
         [Inject] private IUriHelper UriHelper { get; set; }
@@ -18,14 +20,27 @@ namespace Cocorico.Client.Blazor.ComponentModels.Sandwich
         // ReSharper disable once UnusedAutoPropertyAccessor.Local
         [Inject] private ISandwichClient SandwichHttpClient { get; set; }
 
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        [Inject] private IIngredientClient IngredientClient { get; set; }
+
         protected SandwichDto Sandwich { get; private set; } = new SandwichDto();
+
+        protected List<IngredientDto> AvailableIngredients { get; private set; } = new List<IngredientDto>();
+
+        private List<IngredientDto> AddedIngredients { get; } = new List<IngredientDto>();
 
         protected override async Task OnInitAsync()
         {
             try
             {
-                var sandwichResultDto = await SandwichHttpClient.GetAsync(SandwichId);
-                Sandwich = sandwichResultDto;
+                var sandwichDto = await SandwichHttpClient.GetAsync(Id);
+                Sandwich = sandwichDto;
+                AddedIngredients.Clear();
+                AddedIngredients.AddRange(sandwichDto.Ingredients);
+
+                var ingredients = await IngredientClient.GetAllAsync();
+
+                AvailableIngredients.AddRange(ingredients);
             }
             catch (SwaggerException)
             {
@@ -33,10 +48,24 @@ namespace Cocorico.Client.Blazor.ComponentModels.Sandwich
             }
         }
 
+        protected void AddIngredient(IngredientDto ingredient)
+        {
+            AddedIngredients.Add(ingredient);
+            Sandwich.Ingredients = AddedIngredients;
+        }
+
+        protected void RemoveIngredient(IngredientDto ingredient)
+        {
+            AddedIngredients.Remove(ingredient);
+            Sandwich.Ingredients = AddedIngredients;
+        }
+
         protected async Task Edit()
         {
             try
             {
+                Sandwich.Ingredients = AddedIngredients;
+
                 var fileResponse = await SandwichHttpClient.UpdateAsync(Sandwich);
 
                 //TODO: Handle fail

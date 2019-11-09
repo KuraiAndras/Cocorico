1b33b5736e6d36daf67d85ef4de8a20c57eeced4
Kurai András <k.andras9642@gmail.com>
2019-11-09 20:19:35 +0100

    loginview

---
+++ Cocorico.Client.Domain/ViewModels/Authentication/ILoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Cocorico.Shared.Dtos.Authentication;
+
+namespace Cocorico.Client.Domain.ViewModels.Authentication
+{
+    public interface ILoginViewModel
+    {
+        bool ShowLoginFailed { get; }
+        LoginDetails UserLoginDetails { get; }
+        Task LoginUserAsync();
+    }
+}

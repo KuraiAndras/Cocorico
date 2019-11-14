500b6d184c6d4ab1b8e5e196a376508727eeabc2
Kurai András <k.andras9642@gmail.com>
2019-11-14 16:03:26 +0100

    DesignTimeDbContextFactory

---
+++ Cocorico.Server.Domain/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Cocorico.Server.Domain.Models
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CocoricoDbContext>
+    {
+        public CocoricoDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../Cocorico.Server.Restful/appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<CocoricoDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+            return new CocoricoDbContext(builder.Options);
+        }
+    }
+}

a66f1fd38731b8d84b2835d70db40b56aa167ea0
Kurai András <k.andras9642@gmail.com>
2019-11-29 13:52:28 +0100

    Settings Controller uses opening

--- Cocorico.Server.Restful/Controllers/SettingsController.cs
+++ Cocorico.Server.Restful/Controllers/SettingsController.cs
@@ -1,9 +1,12 @@
 ﻿using Cocorico.Server.Domain.Helpers;
+using Cocorico.Server.Domain.Services.Opening;
 using Cocorico.Server.Domain.Services.OrderService;
+using Cocorico.Shared.Dtos.Opening;
 using Cocorico.Shared.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Cocorico.Server.Restful.Controllers
@@ -15,11 +18,17 @@ namespace Cocorico.Server.Restful.Controllers
     public class SettingsController : ControllerBase
     {
         private readonly IOrderRotatingIdService _orderRotatingIdService;
+        private readonly IOpeningService _openingService;
 
-        public SettingsController(IOrderRotatingIdService orderRotatingIdService) =>
+        public SettingsController(
+            IOrderRotatingIdService orderRotatingIdService,
+            IOpeningService openingService)
+        {
             _orderRotatingIdService = orderRotatingIdService;
+            _openingService = openingService;
+        }
 
-        [HttpGet]
+        [HttpGet(nameof(CurrentRangeAsync))]
         public Task<ActionResult<MutableRange>> CurrentRangeAsync()
         {
             var currentRange = _orderRotatingIdService.GetRange();
@@ -30,10 +39,42 @@ namespace Cocorico.Server.Restful.Controllers
             }));
         }
 
-        [HttpPost]
+        [HttpPost(nameof(SetCurrentRange))]
         public ActionResult SetCurrentRange([FromBody] MutableRange newRange)
         {
             _orderRotatingIdService.SetRange(new Range(newRange.Start, newRange.End));
+
+            return new OkResult();
+        }
+
+        [HttpGet(nameof(GetAllOpeningsAsync))]
+        public async Task<ActionResult<ICollection<OpeningDto>>> GetAllOpeningsAsync()
+        {
+            var result = await _openingService.GetAllOpeningsAsync();
+
+            return new ActionResult<ICollection<OpeningDto>>(result);
+        }
+
+        [HttpGet(nameof(IsStoreOpenAsync))]
+        public async Task<ActionResult<bool>> IsStoreOpenAsync()
+        {
+            var result = await _openingService.CanAddOrderAsync(DateTime.Now);
+
+            return new ActionResult<bool>(result);
+        }
+
+        [HttpPost(nameof(AddOpening))]
+        public async Task<ActionResult> AddOpening([FromBody] AddOpeningDto addOpeningDto)
+        {
+            await _openingService.AddOpening(addOpeningDto);
+
+            return new OkResult();
+        }
+
+        [HttpPost(nameof(UpdateOpening))]
+        public async Task<ActionResult> UpdateOpening([FromBody] OpeningDto openingDto)
+        {
+            await _openingService.UpdateOpening(openingDto);
 
             return new OkResult();
         }

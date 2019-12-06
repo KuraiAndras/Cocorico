2f5f1460c3412392dbff352efc075c63cfe64c88
Kurai András <k.andras9642@gmail.com>
2019-12-06 20:28:44 +0100

    Exceptions

---
+++ Cocorico.Domain/Exceptions/InvalidCommandException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Cocorico.Domain.Exceptions
+{
+    public sealed class InvalidCommandException : Exception
+    {
+        public InvalidCommandException()
+        {
+        }
+
+        public InvalidCommandException(string message) : base(message)
+        {
+        }
+
+        public InvalidCommandException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public InvalidCommandException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Cocorico.Domain/Exceptions/StoreClosedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Cocorico.Domain.Exceptions
+{
+    public sealed class StoreClosedException : Exception
+    {
+        public StoreClosedException(string message) : base(message)
+        {
+        }
+
+        public StoreClosedException()
+        {
+        }
+
+        public StoreClosedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public StoreClosedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}

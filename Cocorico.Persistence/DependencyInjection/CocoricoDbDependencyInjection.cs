f8fe556c8952dc7ed4f6ef65396a40152899e17d
Kurai András <k.andras9642@gmail.com>
2019-12-12 23:43:47 +0100

    Persistence DI

---
+++ Cocorico.Persistence/DependencyInjection/CocoricoDbDependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Cocorico.Persistence.DependencyInjection
+{
+    public static class CocoricoDbDependencyInjection
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<CocoricoDbContext>(
+                options => options
+                    .EnableSensitiveDataLogging()
+                    .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+    }
+}

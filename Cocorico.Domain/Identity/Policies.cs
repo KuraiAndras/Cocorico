41ab55cd54ef642623e3330f544e037474af7b8f
Kurai András <k.andras9642@gmail.com>
2019-12-12 23:44:00 +0100

    Added claims, policies

---
+++ Cocorico.Domain/Identity/Policies.cs
@@ -0,0 +1,10 @@
+﻿namespace Cocorico.Domain.Identity
+{
+    public static class Policies
+    {
+        public const string User = "User";
+        public const string Administrator = "Administrator";
+        public const string Customer = "Customer";
+        public const string Worker = "Worker";
+    }
+}

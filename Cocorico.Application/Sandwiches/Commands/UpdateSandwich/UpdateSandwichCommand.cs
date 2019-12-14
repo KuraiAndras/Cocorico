ca409bdcc85e9673b2b60abce60ce3c3aa3c598f
Kurai András <k.andras9642@gmail.com>
2019-12-15 00:09:56 +0100

    Addded sandwich command, queries

---
+++ Cocorico.Application/Sandwiches/Commands/UpdateSandwich/UpdateSandwichCommand.cs
@@ -0,0 +1,13 @@
+﻿using Cocorico.Shared.Dtos.Sandwich;
+using MediatR;
+
+namespace Cocorico.Application.Sandwiches.Commands.UpdateSandwich
+{
+    public sealed class UpdateSandwichCommand : IRequest
+    {
+        public UpdateSandwichCommand(SandwichDto dto) =>
+            Dto = dto;
+
+        public SandwichDto Dto { get; }
+    }
+}
---
+++ Cocorico.Application/Sandwiches/Queries/GetAllSandwich/GetAllSandwichQuery.cs
@@ -0,0 +1,10 @@
+﻿using Cocorico.Shared.Dtos.Sandwich;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Cocorico.Application.Sandwiches.Queries.GetAllSandwich
+{
+    public sealed class GetAllSandwichQuery : IRequest<ICollection<SandwichDto>>
+    {
+    }
+}

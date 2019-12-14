bf3272f7ce0397885646fd35505fefbfea8a3c6d
Kurai András <k.andras9642@gmail.com>
2019-12-14 21:19:16 +0100

    seperate profiles

--- Cocorico.Mappings/DependencyInjection.cs
+++ Cocorico.Mappings/DependencyInjection.cs
@@ -1,12 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
 
 namespace Cocorico.Mappings
 {
     public static class DependencyInjection
     {
         public static void AddMappings(this IServiceCollection services) =>
-            services.AddAutoMapper(Assembly.Load($"{nameof(Cocorico)}.{nameof(Mappings)}"));
+            services.AddAutoMapper(typeof(DependencyInjection).Assembly);
     }
 }
---
+++ Cocorico.Mappings/SandwichProfile.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using AutoMapper;
+using Cocorico.Domain.Entities;
+using Cocorico.Shared.Dtos.Sandwich;
+
+namespace Cocorico.Mappings
+{
+    public sealed class SandwichProfile : Profile
+    {
+        public SandwichProfile()
+        {
+            CreateMap<Sandwich, SandwichDto>()
+                .ForMember(d => d.Ingredients,
+                    o => o.MapFrom(s => s.SandwichIngredients.Select(si => si.Ingredient)));
+            CreateMap<SandwichAddDto, Sandwich>();
+            CreateMap<SandwichDto, Sandwich>();
+        }
+    }
+}

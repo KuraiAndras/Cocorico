fda362705f3c913fd84ad32b552661f66babef5c
Kurai András <k.andras9642@gmail.com>
2019-12-15 17:48:16 +0100

    Query, Command base classes

---
+++ Cocorico.Application/CommandHandlerBase.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Cocorico.Application.Common.Persistence;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cocorico.Application
+{
+    public abstract class CommandHandlerBase<T> : AsyncRequestHandler<T> where T : IRequest
+    {
+        protected CommandHandlerBase(
+            IMediator mediator,
+            IMapper mapper,
+            ICocoricoDbContext context)
+        {
+            Mediator = mediator;
+            Mapper = mapper;
+            Context = context;
+        }
+
+        protected IMediator Mediator { get; }
+        protected IMapper Mapper { get; }
+        protected ICocoricoDbContext Context { get; }
+
+        protected abstract override Task Handle(T request, CancellationToken cancellationToken);
+    }
+}

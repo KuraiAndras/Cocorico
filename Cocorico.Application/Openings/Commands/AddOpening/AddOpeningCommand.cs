b2ba9f73f446a052ae67486b29b43ff560d1cdc9
Kurai András <k.andras9642@gmail.com>
2019-12-15 18:16:12 +0100

    Openings cqrs

---
+++ Cocorico.Application/Openings/Commands/AddOpening/AddOpeningCommand.cs
@@ -0,0 +1,12 @@
+﻿using Cocorico.Shared.Dtos.Opening;
+using MediatR;
+
+namespace Cocorico.Application.Openings.Commands.AddOpening
+{
+    public sealed class AddOpeningCommand : IRequest
+    {
+        public AddOpeningCommand(AddOpeningDto dto) => Dto = dto;
+
+        public AddOpeningDto Dto { get; }
+    }
+}
---
+++ Cocorico.Application/Openings/Commands/UpdateOpening/UpdateOpeningCommand.cs
@@ -0,0 +1,13 @@
+﻿using Cocorico.Shared.Dtos.Opening;
+using MediatR;
+
+namespace Cocorico.Application.Openings.Commands.UpdateOpening
+{
+    public sealed class UpdateOpeningCommand : IRequest
+    {
+        public UpdateOpeningCommand(OpeningDto dto) =>
+            Dto = dto;
+
+        public OpeningDto Dto { get; }
+    }
+}

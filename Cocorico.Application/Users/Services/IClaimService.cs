4444ba89862623558f607123c71aa1d31e376b43
Kurai András <k.andras9642@gmail.com>
2019-12-18 17:39:28 +0100

    register command

---
+++ Cocorico.Application/Users/Services/IClaimService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Cocorico.Application.Users.Services
+{
+    public interface IClaimService
+    {
+        public Task<List<Claim>> GetBasicUserClaimsAsync();
+    }
+}

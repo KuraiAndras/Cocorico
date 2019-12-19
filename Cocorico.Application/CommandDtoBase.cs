7c6564e04b6c44a5b19d1034372ce13ef2b7bb66
Kurai András <k.andras9642@gmail.com>
2019-12-19 12:12:05 +0100

    dto request bases

---
+++ Cocorico.Application/CommandDtoBase.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Cocorico.Application
+{
+    public abstract class CommandDtoBase<TDto> : DtoMessageBase<TDto>, IRequest
+    {
+        protected CommandDtoBase(TDto dto) : base(dto)
+        {
+        }
+    }
+}

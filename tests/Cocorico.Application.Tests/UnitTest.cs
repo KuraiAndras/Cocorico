e95686e6a1baf12a41d080a0446ee871e9098c55
Kurai András <k.andras9642@gmail.com>
2020-04-16 18:14:47 +0200

    Add sonar workflow

--- tests/Cocorico.Application.Tests/UnitTest.cs
+++ tests/Cocorico.Application.Tests/UnitTest.cs
@@ -1,10 +1,16 @@
-﻿using Xunit;
+﻿using Cocorico.Shared.Dtos.Authentication;
+using Xunit;
 
 namespace Cocorico.Application.Tests
 {
     public class UnitTest
     {
         [Fact]
-        public void Test1() => Assert.True(true);
+        public void Test1()
+        {
+            var sut = new UserClaimRequest();
+
+            Assert.NotNull(sut);
+        }
     }
 }
